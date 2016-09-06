5828ec34fc47cdac9ccfd61bdf7c261c9d84588a
Diego Herrera <vermicida@gmail.com>
2016-09-06 22:13:17 +0200

    Updated to rc.6

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,35 +1,33 @@
-
-(function() {
-
-    var config = {
-        map: {
-            "app": "app",
-            "@angular": "node_modules/@angular",
-            "angular2-in-memory-web-api": "node_modules/angular2-in-memory-web-api",
-            "rxjs": "node_modules/rxjs"
-        },
-        packages: {
-            "app": { main: "main.js", defaultExtension: "js" },
-            "angular2-in-memory-web-api": { main: "index.js", defaultExtension: "js" },
-            "rxjs": { defaultExtension: "js" }
-        }
-    };
-
-    var setPackageLoader = System.packageWithIndex
-        ? function(pkg) { config.packages["@angular/" + pkg] = { main: "index.js", defaultExtension: "js" }; }
-        : function(pkg) { config.packages["@angular/" + pkg] = { main: "/bundles/" + pkg + ".umd.js", defaultExtension: "js" }; };
-    
-    ["common",
-     "compiler",
-     "core",
-     "forms",
-     "http",
-     "platform-browser",
-     "platform-browser-dynamic",
-     "router",
-     "router-deprecated",
-     "upgrade"].forEach(setPackageLoader);
-
-    System.config(config);
-
-})();
+(function (global) {
+  System.config({
+    paths: {
+      "npm:": "node_modules/"
+    },
+    map: {
+      app: "app",
+      "@angular/core": "npm:@angular/core/bundles/core.umd.js",
+      "@angular/common": "npm:@angular/common/bundles/common.umd.js",
+      "@angular/compiler": "npm:@angular/compiler/bundles/compiler.umd.js",
+      "@angular/platform-browser": "npm:@angular/platform-browser/bundles/platform-browser.umd.js",
+      "@angular/platform-browser-dynamic": "npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js",
+      "@angular/http": "npm:@angular/http/bundles/http.umd.js",
+      "@angular/router": "npm:@angular/router/bundles/router.umd.js",
+      "@angular/forms": "npm:@angular/forms/bundles/forms.umd.js",
+      "rxjs": "npm:rxjs",
+      "angular2-in-memory-web-api": "npm:angular2-in-memory-web-api"
+    },
+    packages: {
+      app: {
+        main: "./main.js",
+        defaultExtension: "js"
+      },
+      rxjs: {
+        defaultExtension: "js"
+      },
+      "angular2-in-memory-web-api": {
+        main: "./index.js",
+        defaultExtension: "js"
+      }
+    }
+  });
+})(this);

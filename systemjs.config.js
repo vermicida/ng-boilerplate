d071536651c315917c45f3db6ecad301c5a2770f
Diego Herrera <vermicida@gmail.com>
2016-12-28 23:03:20 +0100

    Updated to Angular 2.4.0.

--- systemjs.config.js
+++ systemjs.config.js
@@ -12,7 +12,6 @@
             "@angular/platform-browser-dynamic": "npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js",
             "@angular/http": "npm:@angular/http/bundles/http.umd.js",
             "@angular/router": "npm:@angular/router/bundles/router.umd.js",
-            "@angular/router/upgrade": "npm:@angular/router/bundles/router-upgrade.umd.js",
             "@angular/forms": "npm:@angular/forms/bundles/forms.umd.js",
             "rxjs": "npm:rxjs",
             "angular-in-memory-web-api": "npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js"
